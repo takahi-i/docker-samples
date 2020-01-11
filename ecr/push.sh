7d7bee905ef11247a58f48be438e3061e46d42a9
takahi-i <takahikoito@gmail.com>
2020-01-12 00:44:21 +0900

    Add makefile

--- ecr/push.sh
+++ ecr/push.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+set -o xtrace
+set -e
 
-DOCKER_CONTENT_TRUST=1
-AWS_ACCOUNT_ID=xxxxxxxxxxx
-REGION=ap-northeast-1
+$(aws ecr get-login --no-include-email --region $REGION --profile $ROLE)
 
-$(aws ecr get-login --no-include-email --region ap-northeast-1 --profile takahi-i-2)
+echo REGION: $REGION
+echo ROLE: $ROLE
+echo ID: $AWS_ACCOUNT_ID
 
 docker build -t hello .
 echo "built hello"
-docker tag hello:latest $(AWS_ACCOUNT_ID).dkr.ecr.$(REGION).amazonaws.com/sample:latest
+docker tag hello:latest $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/sample:latest
 echo "taged"
-docker push $(AWS_ACCOUNT_ID).dkr.ecr.$(REGION).amazonaws.com/sample:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/sample:latest
 echo "succeded to push image to ecr"
